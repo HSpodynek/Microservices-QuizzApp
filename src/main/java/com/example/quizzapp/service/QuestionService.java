2819d79f102c62ae77b6dbce37c33d22d0a2c7ef
HubertSpodynek <hspodynek@edu.cdv.pl>
2023-10-04 21:41:57 +0200

    Get data

---
+++ src/main/java/com/example/quizzapp/service/QuestionService.java
@@ -0,0 +1,19 @@
+package com.example.quizzapp.service;
+
+import com.example.quizzapp.Question;
+import com.example.quizzapp.dao.QuestionDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionService {
+
+    @Autowired
+    QuestionDao questionDao;
+
+    public List<Question> getAllQuestions() {
+        return questionDao.findAll();
+    }
+}
