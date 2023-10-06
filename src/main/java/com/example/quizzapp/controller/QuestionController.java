dc2bf144fe47a1fb471d3cb8bc178f09c5efe638
HubertSpodynek <hspodynek@edu.cdv.pl>
2023-10-06 02:22:13 +0200

    Add questions

--- src/main/java/com/example/quizzapp/controller/QuestionController.java
+++ src/main/java/com/example/quizzapp/controller/QuestionController.java
@@ -3,9 +3,7 @@ package com.example.quizzapp.controller;
 import com.example.quizzapp.Question;
 import com.example.quizzapp.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,4 +18,15 @@ public class QuestionController {
     public List<Question> getAllQuestions(){
         return questionService.getAllQuestions();
     }
+    @GetMapping("category/{category}")
+    public List<Question> getQuestionsByCategory(@PathVariable String category){
+        return questionService.getQuestionByCategory(category);
+
+    }
+
+    @PostMapping("add")
+    public String addQuestion(@RequestBody Question question){
+        return questionService.addQuestion(question);
+    }
+
 }
