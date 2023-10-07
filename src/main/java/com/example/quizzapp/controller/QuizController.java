a6f63ca839c6274d8aa251e611bb48a82750d49c
HubertSpodynek <hspodynek@edu.cdv.pl>
2023-10-07 17:18:01 +0200

    Add quiz generate

--- src/main/java/com/example/quizzapp/controller/QuizController.java
+++ src/main/java/com/example/quizzapp/controller/QuizController.java
@@ -1,14 +1,15 @@
 package com.example.quizzapp.controller;
 
 
+import com.example.quizzapp.model.Question;
+import com.example.quizzapp.model.QuestionWrapper;
 import com.example.quizzapp.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("quiz")
@@ -21,4 +22,11 @@ public class QuizController {
     public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title){
         return quizService.createQuiz(category, numQ, title);
     }
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id){
+        return quizService.getQuizQuestions(id);
+    }
+
+
 }
